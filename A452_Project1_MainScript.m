6af6196b3548a35453691bab9c84178d88a1491c
Justin <jtself@calpoly.edu>
2023-10-02 08:40:39 -0700

    Created main script file

---
+++ A452_Project1_MainScript.m
@@ -0,0 +1,20 @@
+%{
+ AERO 452: Spaceflight Dynamics II
+Group Project #1
+Group #21
+Authors: Suren Sanai and Justin Self 
+Main Script here. 
+All functions are separate .m files and MUST be stored in the GitHub local
+folder.
+%}
+
+% Housekeeping
+clear all; close all; clc;
+
+% Constants
+mu = 398600; % km3/s2
+r_earth = 6378; % km
+
+%% Section 1
+
+%% Section 2
