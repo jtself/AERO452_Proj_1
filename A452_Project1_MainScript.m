b33bf3488cc3488d34e6e83523d511a1bc5e920f
surensanai <146789526+surensanai@users.noreply.github.com>
2023-10-05 10:48:16 -0700

    UNDO ERASE

--- A452_Project1_MainScript.m
+++ A452_Project1_MainScript.m
@@ -1,1 +1,26 @@
 
+%{
+ AERO 452: Spaceflight Dynamics II
+Group Project #1
+Group #21
+Authors: Suren Sanai and Justin Self 
+Main Script here. 
+All functions are separate .m files and MUST be stored in the GitHub local
+folder.
+%}
+
+% Housekeeping
+clear all; close all; clc;
+
+% Constants
+mu = 398600; % km3/s2
+r_earth = 6378; % km
+
+%% Section 1
+
+g = pi^2;  
+
+% How are you? 
+
+
+%% Section 2
